a47b9368b428070c821b0fc58588827a2d1ea2d0
seth.yang <sethyang@119app.com>
2016-02-23 03:11:40 +0800

    update

---
+++ modules/cloud/src/com/cng/cloud/util/EventListTypeToken.java
@@ -0,0 +1,12 @@
+package com.cng.cloud.util;
+
+import com.cng.cloud.data.Event;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
+/**
+ * Created by game on 2016/2/23
+ */
+public class EventListTypeToken extends TypeToken<List<Event>> {
+}
--- modules/cloud/src/com/cng/cloud/web/UploadServlet.java
+++ modules/cloud/src/com/cng/cloud/web/UploadServlet.java
@@ -1,5 +1,13 @@
 package com.cng.cloud.web;
 
+import com.cng.cloud.data.Event;
+import com.cng.cloud.data.Result;
+import com.cng.cloud.service.IEventService;
+import com.cng.cloud.util.EventListTypeToken;
+import com.google.gson.Gson;
+import org.dreamwork.gson.GsonHelper;
+import org.dreamwork.persistence.ServiceFactory;
+import org.dreamwork.util.IOUtil;
 import org.dreamwork.util.StringUtil;
 
 import javax.servlet.ServletException;
@@ -7,11 +15,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 /**
  * Created by game on 2016/2/23
  */
 public class UploadServlet extends HttpServlet {
+    private static final String CONTENT_TYPE = "applications/json";
+    private Type type;
+
+    @Override
+    public void init () throws ServletException {
+        super.init ();
+        type = new EventListTypeToken ().getType ();
+    }
+
     @Override
     protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String hostId = request.getQueryString ();
@@ -20,6 +39,24 @@ public class UploadServlet extends HttpServlet {
             return;
         }
 
+        Result<Object> result = new Result<Object> ();
+        Gson g = GsonHelper.getGson (true, true);
+        try {
+            byte[] buff = IOUtil.read (request.getInputStream ());
+            String content = new String (buff, "utf-8");
+            List<Event> list = g.fromJson (content, type);
+            for (Event e : list)
+                e.setHostId (hostId);
 
+            IEventService service = (IEventService) ServiceFactory.getBean ("eventService");
+            service.save (list);
+
+            result.setState ("ok");
+        } catch (Exception ex) {
+            result.setState ("fail");
+            ex.printStackTrace ();
+        }
+        response.setContentType (CONTENT_TYPE);
+        response.getWriter ().write (g.toJson (result));
     }
 }
